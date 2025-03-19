10c6c714e63bd300e47de0df95124dcad6985675
kanishkabhardwaj12 <143208189+kanishkabhardwaj12@users.noreply.github.com>
2025-03-19 17:14:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-import joblib  # For saving the model
+import joblib  
 
 csv_files = [
     'dataset-of-60s.csv', 'dataset-of-70s.csv', 'dataset-of-80s.csv',
@@ -24,19 +24,15 @@ for file in csv_files:
     X = df[selected_features]
     y = df.iloc[:, -1]
     
-    # Train-test split
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
-    # Feature scaling
     scaler = StandardScaler()
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)
     
-    # Model training
     model = RandomForestClassifier(random_state=42)
     model.fit(X_train, y_train)
     
-    # Model evaluation
     y_pred = model.predict(X_test)
     accuracy = accuracy_score(y_test, y_pred)
     
